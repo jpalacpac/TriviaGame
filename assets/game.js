4606bf97f1851a3be2d1a4fb1fd99be20cf62b74
unknown <jpalacpac07@gmail.com>
2017-06-22 17:30:08 -0700

    updating files

--- assets/game.js
+++ assets/game.js
@@ -12,8 +12,8 @@ var missedTotal = 0;
 var correctAnswers = [
 "C. Taxi Driver", 
 "B. The Silence of the Lambs", 
-"A. Sixth Sense", 
-"C. Scarface", 
+"A. Fight Club", 
+"C. Dumb and Dumber", 
 "D. The Usual Suspects", 
 "A. Sudden Impact", 
 "B. 2001: A Space Odessey", 
@@ -22,8 +22,8 @@ var correctAnswers = [
 var questionArray = [
 '"You talking to me?"', 
 '"I\'m having an old friend for dinner."', 
-'"I see dead people."', 
-'"Say "HELLO!" to my little friend!"', 
+'"The things you own end up owning you."', 
+'"Wanna hear the most annoying sound in the world?"', 
 '"The greatest trick the devil ever pulled was convincing the world he didn\'t exist."', 
 '"Go ahead, make my day."', '"Open the pod bay doors, please, HAL"', 
 '"They call it a Royale with cheese."'];
@@ -31,8 +31,9 @@ var questionArray = [
 var answerArray = [
 ["Anger Management", "Last Man Standing", "Taxi Driver", "Save The Last Dance"], 
 ["Murder She Wrote","The Silence of the Lambs","Maid to Kill","Dead Man Dessert"], 
-["Sixth Sense", "It Comes in the Night", "Dead Rising", "Tokyo Horror Story"], 
-["Terminator","Goodfellas","Scarface","Casino"], ["Let Me In", "When Evil Comes", "Oceans Eleven", "The Usual Suspects"], 
+["Fight Club", "Takers", "Aces High", "The Godfather"], 
+["Big Mommas House","Goodfellas","Dumb and Dumber","Eurotrip"], 
+["Let Me In", "When Evil Comes", "Oceans Eleven", "The Usual Suspects"], 
 ["Sudden Impact","The Good, The Bad, and The Ugly","Road to the West","Lethal Weapon"], 
 ["First Contact", "2001: A Space Odessey", "A.I", "Prometheus"], 
 ["Ride or Die","Welcome to Good Burger","Hard Knock\'s","Pulp Fiction"]];
@@ -40,9 +41,9 @@ var answerArray = [
 var gifArray = [
 '<iframe class="center-block img-right" src="https://media.giphy.com/media/3o6gEaWjgBft7euDPW/giphy.gif" width="480" height="480" frameBorder="0">', 
 '<iframe class="center-block img-right" src="https://media.giphy.com/media/CdG1VUd9dXr68/giphy.gif" width="480" height="480" frameBorder="0">', 
-'<iframe class="center-block img-right"  src="https://media.giphy.com/media/DvtsYOKrqPZg4/giphy.gif" width="480" height="480" frameBorder="0">', 
-'<iframe class="center-block img-right" src="https://media.giphy.com/media/gYkga3bZav66I/giphy.gif" width="480" height="480" frameBorder="0">', 
-'<iframe class="center-block img-right" src="https://media.giphy.com/media/8qwYGJVIOWhSo/giphy.gif" width="480" height="480" frameBorder="0">', 
+'<iframe class="center-block img-right"  src="https://media.giphy.com/media/lYSJQNnTI7aow/giphy.gif" width="480" height="480" frameBorder="0">', 
+'<iframe class="center-block img-right" src="https://media.giphy.com/media/7B6hvCLsQQBPO/giphy.gif" width="480" height="480" frameBorder="0">', 
+'<iframe class="center-block img-right" src="https://media.giphy.com/media/d7uu8aGL20QLe/giphy.gif" width="480" height="480" frameBorder="0">', 
 '<iframe class="center-block img-right" src="https://media.giphy.com/media/l41YmgdpcJwUngcXm/giphy.gif" width="480" height="480" frameBorder="0">', 
 '<iframe class="center-block img-right" src="https://media.giphy.com/media/CUF595USvDMnS/giphy.gif" width="480" height="480" frameBorder="0">', 
 '<iframe class="center-block img-right" src="https://media.giphy.com/media/fKBh0mt02CypW/giphy.gif" width="480" height="480" frameBorder="0">'];
@@ -103,7 +104,7 @@ function winResult() {
 function lossResult() {
 	wrongTotal++;
 	gameHTML = "<p class='text-center timer-p'>Time Left: <span class='timer'>" + 
-	counter + "</span></p>" + "<p class='text-center'>Wrong!" +  
+	counter + "</span></p>" + "<p class='text-center'>Wrong! Correct answer is: " + correctAnswers[questionCounter] +
 	'<iframe class="center-block img-right" src="https://media.giphy.com/media/hPPx8yk3Bmqys/giphy.gif" width="480" height="500" frameBorder="0">';
 	$(".mainArea").html(gameHTML);
 	setTimeout(wait, 4000); 
